7a9d7d18f2838a097d4914f2600eba4f596d5278
111062118 <jiangdumpling@gmail.com>
2025-01-05 12:50:18 +0800

    complete lab00

--- LAB/Lab00/Lab/src/js/index.js
+++ LAB/Lab00/Lab/src/js/index.js
@@ -1,10 +1,16 @@
 var mySwitch;
 var myBulb;
+
+// an event listener for the switch
 window.onload = function () {
+  // get the HTML ID: switch and the bulb
   mySwitch = document.getElementById('switch');
   myBulb = document.getElementById('bulb');
 };
+
+// function to switch the light on and off
 function switchLight() {
+  // check if the switch is on or off
   if (mySwitch.checked)
     myBulb.src = 'src/img/bulb_on.png';
   else
